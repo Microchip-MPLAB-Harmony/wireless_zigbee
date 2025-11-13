cca50edd0d3d72695183ca3dfed7774c741ce40c
Viswanadham Kotla <Viswanadham.Kotla@Microchip.com>
2025-11-13 15:45:41 +0530

    [WSGREL-72] - module Update

--- config/module.py
+++ config/module.py
@@ -45,18 +45,6 @@ def loadModule():
                            'PIC32WM_BZ3601',
                            'PIC32WM_BZ3602',
                           }
-
-    pic32cx_bz6_family = {  'PIC32CX2051BZ62132',
-                            'PIC32CX2051BZ62064',
-                            'PIC32CX2051BZ66048',
-                            'WBZ653',
-                            'WBZ652',
-                            'WBZ651',
-                            'PIC32WM_BZ6204',
-                            'PIC32WM_BZ6203',
-                            'PIC32WM_BZ6602',                            
-                            'PIC32CX2051BZ62132_FPGA',
-                        }
     
     processor = Variables.get('__PROCESSOR')
     print('processor={}'.format(processor))
@@ -66,5 +54,3 @@ def loadModule():
         execfile(Module.getPath() + '/config/module_zigbee_bz2.py')
     elif( (processor in pic32cx_bz3_family)or (processor in pic32cx_bz36_family)):
         execfile(Module.getPath() + '/config/module_zigbee_bz3.py')
-    elif( processor in pic32cx_bz6_family):
-        execfile(Module.getPath() + '/config/module_zigbee_bz6.py')
